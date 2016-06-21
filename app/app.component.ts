d05616fef0c4a571ee2f4ebc949e5891101dabc7
Ned <nedfaulhaber@gmail.com>
2016-06-21 10:47:51 -0700

    enable and configure router and add landing route and about route

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from 'angular2/core';
+import {RouteConfig} from 'angular2/router';
+
+import {LandingComponent} from './landing.component';
+import {AboutComponent} from './about.component';
+
+@RouteConfig([
+  { path: '/landing', name: 'Landing', component: LandingComponent, useAsDefault: true },
+  { path: '/about', name: 'About', component: AboutComponent }
+  { path: '/*other', name: 'Other', redirectTo: ['Landing']  }
+])
 
 @Component({
   selector: 'my-app',
